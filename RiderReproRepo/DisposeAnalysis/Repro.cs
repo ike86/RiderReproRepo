24917c882c15930bf0e9805a662191d3ccca8140
Pfeff Ákos György <ike86@users.noreply.github.com>
2024-05-16 11:14:11 +0200

    add repro of allegedly false-positive "Return value of this invocation is never disposed"

---
+++ RiderReproRepo/DisposeAnalysis/Repro.cs
@@ -0,0 +1,37 @@
+﻿using System.Reactive.Disposables;
+using JetBrains.Annotations;
+
+namespace RiderReproRepo.DisposeAnalysis;
+
+public sealed class Repro : IDisposable
+{
+    private readonly CompositeDisposable disposer = new();
+    
+    public Repro()
+    {
+        DisposableByCtor = new Disposable().DisposeWith(disposer);
+        DisposableByFactory = new DisposableFactory()
+            .Create()
+            .DisposeWith(disposer);
+    }
+
+    public Disposable DisposableByCtor { get; }
+
+    public Disposable DisposableByFactory { get; }
+
+    public void Dispose() => disposer.Dispose();
+}
+
+[MustDisposeResource]
+public sealed class Disposable : IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
+
+public class DisposableFactory
+{
+    [MustDisposeResource]
+    public Disposable Create() => new Disposable();
+}
